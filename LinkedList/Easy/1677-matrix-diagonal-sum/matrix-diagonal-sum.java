bb863714cbf1ffa476a11aff4fa64c0cbba11034
Om_shingare <omkarshingare.cs@gmail.com>
2026-08-14 23:09:57 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (32.31%) - LeetSync

---
+++ LinkedList/Easy/1677-matrix-diagonal-sum/matrix-diagonal-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int diagonalSum(int[][] mat) {
+        int sum = 0;
+        // trying leetsync
+        //hehe
+        for (int i = 0; i< mat.length ; i++){
+        
+            sum += mat[i][i];
+    
+             if(i != mat.length-i-1)
+            sum += mat[i][mat.length-1-i];
+        }
+        return sum;
+    }
+   
+}
