33bd95d97f34c6d1eca08cd645f78db490cc3f27
Om_shingare <omkarshingare.cs@gmail.com>
2026-08-22 23:47:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (70.60%) - LeetSync

--- Java/Problems/1013-fibonacci-number/fibonacci-number.java
+++ Java/Problems/1013-fibonacci-number/fibonacci-number.java
@@ -1,14 +1,18 @@
 class Solution {
     public int fib(int n) {
 
-        if (n==0 || n==1){
+        if (n<=1){
             return n;
         }
 
-        int fnm1 = fib(n-1);
-        int fnm2 = fib(n-2);
-        int fn = fnm1 + fnm2;
-        return fn;
+        int fnm1 =1;
+        int fnm2 = 0;
+        for (int i = 2 ;i<= n; i++){
+            int curr = fnm1 + fnm2 ;
+            fnm2 = fnm1;
+            fnm1 = curr;
+        }
+        return fnm1;
          
         
     }
